b30e29ef5b6459b9f80d660295b0732ff6b2df3e
CrackThaDom <gotroyahoy@outlook.com>
2022-12-17 10:00:08 -0600

    Challenge method now achieves objective of displaying lowercase words in reverse

--- day_twenty.py
+++ day_twenty.py
@@ -20,11 +20,11 @@ is how your output should look:
 ['gninrAel', 'tUb', 'uoY', 'yLppa', 'flesRuoy', 'eVeihca']"""
 
 
-def challenge(string:str) -> list:
-	return [word[::-1] for word in string.split(" ") for letter in word if letter.isupper()]
+def reverse_lowercase(string:str) -> list:
+	return [word[::-1] for word in string.split(" ") if not any(ch.isupper() for ch in word)]
 
 
 str1 = 'leArning is hard, bUt if You appLy youRself you can achieVe success'
 
-print(challenge(str1))
+print(reverse_lowercase(str1))
 print(capitalize_each_word(str1))
