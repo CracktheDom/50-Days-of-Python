0bf2d330aba48bd19404429c82dd0ad35b478512
CrackThaDom <gotroyahoy@outlook.com>
2023-02-17 00:53:35 -0600

    completed the extra challenge, used max function with key=len parameter to find longest value in dict

--- extra_day01.py
+++ extra_day01.py
@@ -5,15 +5,13 @@ as an argument and returns the first longest value of the
 dictionary. For example, the following dictionary should return
 – apple as the longest value."""
 
-# TODO: Need to finish returning longest value, not length of value
 
-def longest_value(dictLen:dict) -> str:
-	max_value_length_list = [len(value) for value in dictLen.values()]
-	max_value = max(max_value_length_list)
-	max_value_index = max_value_length_list.index(max_value)
-	return max_value
+def longest_value(dictLen: dict) -> str:
+    max_value = max(dictLen.values(), key=len)
+    return max_value
 
 
-fruits = {'fruit': 'durian', 'colors':'aquamarine', 'flowers': 'chrysanthemums'}
+fruits = {'fruit': 'durian', 'colors': 'aquamarine',
+          'flowers': 'chrysanthemums'}
 
 print(longest_value(fruits))
