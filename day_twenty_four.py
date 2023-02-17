6103106a9f26988d6b9beae25a02a7b127a7bfe5
CrackThaDom <gotroyahoy@outlook.com>
2023-02-17 01:04:34 -0600

    completed both challenges; no need for boolean in average_calories function, using list for total variable instead of int will allow for tracking number of entries/elements in order to calculate the mean of elements for nested_lists, iterated thru list arguments to create a list of lists

--- day_twenty_four.py
+++ day_twenty_four.py
@@ -3,17 +3,20 @@
 """Create a function called average_calories that calculates the
 average calories intake of a user. The function should ask the user
 to input their calories intake for any number of days and once they
-hit ‘done’ it should calculate and return the average intake."""
+hit 'done' it should calculate and return the average intake."""
 
 
 def average_calories():
-	calories = ''
-	moreQuestions = True
-	total = 0
-	while moreQuestions and calories != 'done':
-		calories = input("What was your caloric intake? ")
-		total += float(calories)
+    calories = ''
+    total = []
+    while calories != 'done':
+        calories = input("What was your caloric intake? ")
+        if calories != 'done' and calories.isnumeric():
+            total.append(float(calories))
+        else:
+            print("Please enter numeric values.")
 
+    return sum(total) / len(total) if len(total) > 0 else 0
 
 
 """Extra Challenge: Create a Nested List
@@ -25,8 +28,9 @@ Your code should return: [[1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]]"""
 
 
 def nested_lists(*args) -> list:
-	return [arg for arg in args]
+    return [arg for arg in args]
 
 
-assert nested_lists([1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]) == [[1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]]
+assert nested_lists([1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]) == [
+    [1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]]
 print(nested_lists([1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]))
