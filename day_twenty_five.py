851650aae12737e8f16f2aa860a948e1d79c170f
CrackThaDom <gotroyahoy@outlook.com>
2023-02-17 00:55:30 -0600

    completed extra challenge, need to complete first challenge

--- day_twenty_five.py
+++ day_twenty_five.py
@@ -8,8 +8,9 @@ should return True. If not, it should return False. For example,
 
 
 def all_the_same(str_list_or_tuple) -> bool:
-	# TODO: iterate thru list, str, tuple
-	pass
+    # TODO: iterate thru list, str, tuple
+    pass
+
 
 """Extra Challenge: Reverse a String
 str1 = "the love is real"
@@ -17,8 +18,9 @@ Write a function called read_backwards that takes a string as
 an argument and reverses it. For example, the string above
 should return: "real is love the"""
 
+
 def read_backwards(string) -> str:
-	return ' '.join(string.split(' ')[::-1])
+    return ' '.join(string.split(' ')[::-1])
 
 
 print(read_backwards("The arc of a life well lived."))
