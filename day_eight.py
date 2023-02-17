6cfe5804ae69c9db5be3311a9fdeabef733be315
CrackThaDom <gotroyahoy@outlook.com>
2023-02-16 23:58:24 -0600

    formatting file via LSP

--- day_eight.py
+++ day_eight.py
@@ -6,9 +6,11 @@ difference between the largest even number in the list and the
 smallest odd number in the list. For example, if you pass
 [1,2,4,6] as an argument the function should return 6 -1= 5."""
 
+
 def odd_even(numbers: list) -> int:
-	odds = [num for num in numbers if num % 2 == 1]
-	evens = [num for num in numbers if num % 2 == 0]
-	return(max(evens) - min(odds))
+    odds = [num for num in numbers if num % 2 == 1]
+    evens = [num for num in numbers if num % 2 == 0]
+    return (max(evens) - min(odds))
+
 
 print(odd_even([num for num in range(5, 26, 5)]))
