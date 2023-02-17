3d5ad5d6e522ca5378251eebf1605e68a30f1ab4
CrackThaDom <gotroyahoy@outlook.com>
2023-02-16 23:59:01 -0600

    formatting file via LSP

--- day_nine.py
+++ day_nine.py
@@ -5,9 +5,10 @@ numbers and returns the biggest odd number in the list. For
 example, if you pass ‘23569’ as an argument, your function
 should return 9. Use list comprehension."""
 
-def biggest_odd(string:str) -> int:
-	num_list = list(map(int, string))
-	odd_list = [odd for odd in num_list if odd % 2 == 1]
-	return max(odd_list)
+
+def biggest_odd(string: str) -> int:
+    num_list = list(map(int, string))
+    return max([odd for odd in num_list if odd % 2 == 1])
+
 
 print(biggest_odd('324856787'))
