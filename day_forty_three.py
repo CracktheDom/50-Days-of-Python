458964717bf9a0aa37f24c4219885842b45f087d
CrackThaDom <gotroyahoy@outlook.com>
2023-02-20 14:53:11 -0600

    initial commit

---
+++ day_forty_three.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+
+"""Write a function called student_marks that records marks
+achieved by students in a test. The function should ask the user
+to input the name of the student and then ask the user to input
+the marks achieved by the student. The information should be
+stored in a dictionary. The name is the key and the marks is the
+value. When the user enters done, the function should return a
+dictionary of names and values entered."""
+
+
+def student_marks() -> dict:
+    isFinished = False
+    grade_book = {}
+    while not isFinished:
+        student_name = input("What is the student's name?  ")
+        marks = input(f"Enter {student_name}'s marks:  ")
+        grade_book[student_name] = marks
+        are_you_done = input(
+            "Enter 'done' if you are finished entering student marks  ")
+        if are_you_done == 'done':
+            isFinished = True
+
+    return grade_book
+
+
+print(student_marks())
