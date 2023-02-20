1bf115a54d58a0a2594a42a5bda077f1808142d1
CrackThaDom <gotroyahoy@outlook.com>
2023-02-20 05:55:20 -0600

    reformatted file via LSP

--- extra_day02.py
+++ extra_day02.py
@@ -9,18 +9,16 @@ return "No duplicates". For example, the list of fruits below
 should return apple as a duplicate and list of names should
 return "no duplicates"."""
 
-def check_duplicates(str_list:list) -> str:
-	duplicates = []
-	for i in range(len(str_list)):
-		if str_list.count(str_list[i]) >= 2 and str_list[i] not in duplicates:
-			duplicates.append(str_list[i])
-		elif str_list.count(str_list[i]) < 2:
-			continue
 
-	if duplicates == []:
-		return f"no duplicates"
-	else:
-		return duplicates
+def check_duplicates(str_list: list):
+    duplicates = []
+    for elem in str_list:
+        if str_list.count(elem) >= 2 and elem not in duplicates:
+            duplicates.append(elem)
+
+    return f"no duplicates" if len(duplicates) == 0 else duplicates
+
 
 print(check_duplicates(['1', '2', '3', '4', '97']))
-print(check_duplicates(['apples', 'banana', 'guava', 'banana', 'lemon', 'lime', 'kiwi', 'dragonfruit', 'mango', 'lime']))
+print(check_duplicates(['apples', 'banana', 'guava', 'banana',
+      'lemon', 'lime', 'kiwi', 'dragonfruit', 'mango', 'lime']))
