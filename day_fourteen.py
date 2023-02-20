5774e6b05c2affd4955126549517c2a941b38468
CrackThaDom <gotroyahoy@outlook.com>
2023-02-20 15:20:10 -0600

    developed a more thorough programmatic solution that considers different types of nested lists

--- day_fourteen.py
+++ day_fourteen.py
@@ -7,7 +7,11 @@ dimension list. For example [[2,4,5,6]] should return
 
 
 def flat_list(nested: list) -> list:
-    return nested[0]
+    flat = []
+    for inner_list in nested:
+        for elem in inner_list:
+            flat.append(elem)
+    return flat
 
 
 print(flat_list([[2, 4, 5, 6, 78, 24, 11, 89]]))
