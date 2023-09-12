aa4c045367211df6e28ade411684ee0283397b0a
CrackThaDom <85846263+CracktheDom@users.noreply.github.com>
2023-09-12 15:22:01 -0500

    Refactor repeated_name with Counter object, refactor sort_names & add docstrings in day_thirty.py

--- day_thirty.py
+++ day_thirty.py
@@ -4,12 +4,13 @@
 repeated name in the following list.
 name = ["John", "Peter", "John", "Peter", "Jones", "Peter"]"""
 
+from collections import Counter
+
 
 def repeated_name():
-    names = ["John", "John", "John", "Peter",
-             "John", "Peter", "Jones", "Peter"]
-    nameDict = {k: names.count(k) for k in names}
-    return max(nameDict, key=nameDict.get)
+    names = ["John", "John", "John", "Peter", "John", "Peter", "Jones", "Peter"]
+    nameDict = Counter(names)
+    return nameDict.most_common(1)[0][0]
 
 
 """Extra Challenge: Sort by Last Name
@@ -27,17 +28,49 @@ how your code output should look:
 'Knowles Beyonce']
 Write a function called sorted_names"""
 
+from typing import List
 
-def sort_names(fullnames: list):
-    return sorted(
-        [f'{name[name.index(" ") + 1:]} {name[:name.index(" ")]}'
-         for name in fullnames]
-    )
+
+def sort_names(fullnames: List[str]) -> List[str]:
+    """
+    Sorts a list of full names based on the first letter of the second word and
+    returns a new list with the first and second words of each name switched.
+
+    Args:
+        fullnames (List[str]): A list of full names (each name as a string).
+
+    Returns:
+        List[str]: A new list of full names with the first and second words
+        switched, sorted based on the first letter of the second word.
+    """
+
+    # Sort list by the first letter of the second word
+    sorted_list = sorted(fullnames, key=lambda x: x.split()[1][0])
+
+    # Switch the first word and second word of each element
+    switched_list = [" ".join(name.split()[::-1]) for name in sorted_list]
+
+    return switched_list
+
+    # return sorted(
+    #     [f'{name[name.index(" ") + 1:]} {name[:name.index(" ")]}' for name in fullnames]
+    # )
 
 
 print(repeated_name())
-print(sort_names(['Richard Greer', 'Judy Greer', 'Pam Grier',
-                  'Janelle Monae', 'Emily Blunt', 'Beyonce Knowles',
-                  'Alicia Keys', 'Katie Perry', 'Chris Brown', 'Tom Cruise']
-                 )
-      )
+print(
+    sort_names(
+        [
+            "Richard Greer",
+            "Judy Greer",
+            "Pam Grier",
+            "Janelle Monae",
+            "Emily Blunt",
+            "Beyonce Knowles",
+            "Alicia Keys",
+            "Katie Perry",
+            "Chris Brown",
+            "Tom Cruise",
+        ]
+    )
+)
