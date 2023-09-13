f9664a66f42a5d2b98f27a4d2ab7971443e42a5f
CrackThaDom <85846263+CracktheDom@users.noreply.github.com>
2023-09-13 02:40:48 -0500

    Add type checking to convert_add in day_two.py

--- day_two.py
+++ day_two.py
@@ -6,8 +6,11 @@ example ['1', '3', '5'] should be converted to [1, 3, 5] and
 summed to 9."""
 
 
-def convert_add(list_str: list) -> int:
+from typing import List
+
+
+def convert_add(list_str: List[str]) -> int:
     return sum(map(int, list_str))
 
 
-print(convert_add(['2', '5', '7', '1', '8']))
+print(convert_add(["2", "5", "7", "1", "8"]))
