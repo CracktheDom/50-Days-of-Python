411eaf2c296bf6647b79bee011ef192da383989d
CrackThaDom <85846263+CracktheDom@users.noreply.github.com>
2023-09-13 02:37:46 -0500

    Refactor & add docstring to equal_strings in day_eleven.py

--- day_eleven.py
+++ day_eleven.py
@@ -8,17 +8,23 @@ example, 'love' and 'evol' should return True."""
 
 
 def equal_strings(str_one: str, str_two: str) -> bool:
-    isEqual = bool
-    if len(str_one) == len(str_two):
-        for char in str_one:
-            if char not in str_two:
-                isEqual = False
-                break
-            elif char in str_two:
-                isEqual = True
-    else:  # strings are not the same length
-        isEqual = False
-    return isEqual
+    """
+    Compare two strings for equality based on their lengths and reverse order.
+
+    This function takes two strings as input and returns True if they have the
+    same length and are equal when one of them is reversed. Otherwise, it
+    returns False.
+
+    Args:
+        str_one (str): The first input string.
+        str_two (str): The second input string.
+
+    Returns:
+        bool: True if the strings are equal in length and when one is reversed,
+        False otherwise.
+    """
+    return len(str_one) == len(str_two) and str_one == str_two[::-1]
 
 
-print(equal_strings('baffle', 'battle'))
+assert equal_strings("baffle", "battle") is False
+assert equal_strings("love", "evol") is True
