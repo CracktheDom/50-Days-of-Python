2ea888e9706e68ab5809ae702e527eb0fbd697ca
CrackThaDom <85846263+CracktheDom@users.noreply.github.com>
2023-09-12 20:34:26 -0500

    Refactor & add comments, docstrings to odd_even in day_eight.py

--- day_eight.py
+++ day_eight.py
@@ -8,9 +8,57 @@ smallest odd number in the list. For example, if you pass
 
 
 def odd_even(numbers: list) -> int:
-    odds = [num for num in numbers if num % 2 == 1]
-    evens = [num for num in numbers if num % 2 == 0]
-    return (max(evens) - min(odds))
+    """
+    Calculate the difference between the maximum even number
+    and the minimum odd number in a given list of numbers.
+
+    Args:
+        numbers (list): A list of integers.
+
+    Returns:
+        int: The difference between the maximum even number and
+             the minimum odd number. If there are no odd or even
+             numbers in the list, return 0. If there are only even
+             numbers, return the maximum even number. If there are
+             only odd numbers, return the negative of the minimum odd
+             number.
+
+    Example:
+        >>> odd_even([1, 2, 3, 4, 5])
+        3
+        >>> odd_even([2, 4, 6, 8])
+        8
+        >>> odd_even([1, 3, 5, 7])
+        -1
+        >>> odd_even([])
+        0
+    """
+    odds = []
+    evens = []
+
+    # Separate the input numbers into odd and even lists
+    for elem in numbers:
+        if elem % 2 == 0:
+            evens.append(elem)
+        else:
+            odds.append(elem)
+
+    # Calculate the result based on the odd and even lists
+    if len(odds) == 0 and len(evens) == 0:
+        return 0
+    elif len(odds) == 0:
+        return max(evens)
+    elif len(evens) == 0:
+        return -(min(odds))
+
+    # Calculate the difference between the max even and min odd
+    return max(evens) - min(odds)
 
 
-print(odd_even([num for num in range(5, 26, 5)]))
+# Test cases
+assert odd_even([num for num in range(5, 26, 5)]) == 15
+assert odd_even([1, 2, 4, 6]) == 5
+assert odd_even([1, 3, 5, 7]) == -1
+assert odd_even([2, 4, 6, 8]) == 8
+assert odd_even([]) == 0
+assert odd_even([num for num in range(5, 25, 2)]) == -5
